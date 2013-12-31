88bcbbe3e0a318397d7a4e4b4d0819aa5c5b62af
macheteshuffle <jeff.li029@gmail.com>
2013-12-31 13:12:28 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeFit1::Application.config.secret_key_base = '8bb06374753cb7793a3318c1841d433bfca062356e1131a65c61332d41811aaaace9745a14e4237f7ddd6d0144df0240d89cca2957022ee8c65857baa99345cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WeFit1::Application.config.session_store :cookie_store, key: '_we_fit_1_session'
