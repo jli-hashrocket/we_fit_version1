1dad69aa90f9f4eaf123b4f57ab811517eca1949
macheteshuffle <jeff.li029@gmail.com>
2014-01-02 16:08:23 -0500

    seeded Activities table

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,5 @@ require 'csv'
 datafile = Rails.root + 'db/data/activities.csv'
 
 CSV.foreach(datafile, headers: true) do |row|
-  binding.pry
-  activity = Employee.find_or_create_by(name: row[0]
+  Activity.find_or_create_by(name: row["name"])
 end
