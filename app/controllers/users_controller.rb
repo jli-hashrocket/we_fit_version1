050fc545e4ebf55fa8999a38caf35362afc7e889
macheteshuffle <jeff.li029@gmail.com>
2014-01-03 13:06:07 -0500

    working on stack level too deep issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :username, :email, :bio, :photo, :location, :gender, :preferred_gender, :user_activity_id, :activity_ids => [])
+      params.require(:user).permit(:first_name, :last_name, :username, :email, :bio, :photo, :location, :gender, :preferred_gender, :activity_ids => [])
     end
 end
