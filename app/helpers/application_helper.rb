cfa8432756c3e8d53690dc7fccf1b64bb0ecbbd4
macheteshuffle <jeff.li029@gmail.com>
2014-01-08 13:29:40 -0500

    passed browse members acceptance test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def activity_checkbox_checked_value_for(activity)
+    query = params[:q] || {}
+    user_activity_ids = query[:user_activities_activity_id_eq_any] || []
+
+    user_activity_ids.include?(activity.id)
+  end
 end
