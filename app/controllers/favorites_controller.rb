1f45d815d68cdd6a65fe85780483c1b1c521dd97
macheteshuffle <jeff.li029@gmail.com>
2014-01-15 12:18:47 -0500

    worked on favorite deletion and css

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,18 +2,22 @@ class FavoritesController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-     @favorites = current_user.favorite_users
+     @favorites = current_user.favorites
+     @user = current_user
     # @favorites = Favorite.get_favorites(current_user.id)
   end
 
   def create
-    @favorite = current_user.favorites.build(favorited_id: params[:favorited_id])
+    @favorite = current_user.favorites
+      .build(favorited_id: params[:favorited_id])
 
     respond_to do |format|
       if @favorite.save
         format.html { redirect_to 'index', notice: 'Added to Favorites' }
-        format.js { flash.now[:notice] = 'Added to Favorites!'}
-        format.json { render action: 'index', status: :created, location: @favorite}
+        format.js { flash.now[:notice] = 'Added to Favorites!' }
+        format.json do
+          render action: 'index', status: :created, location: @favorite
+        end
       else
         format.html { render action: 'index', notice: 'Cannot add to Favorites' }
         format.js { flash.now[:alert] = 'Could not be added to Favorites!' }
@@ -24,8 +28,9 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-    current_user.favorite_users.destroy(params[:id])
-    redirect_to 'index', notice: 'Removed from Favorites'
+    current_user.favorites.destroy(params[:id])
+    redirect_to user_favorites_path(current_user),
+      notice: 'Removed from Favorites'
   end
 
 end
