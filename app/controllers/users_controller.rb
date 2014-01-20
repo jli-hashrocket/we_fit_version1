00e99a1e70f033fbfdfc64053f2a7fc8e8ed1280
macheteshuffle <jeff.li029@gmail.com>
2014-01-20 18:20:10 -0500

    fixed google map api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,13 @@ class UsersController < ApplicationController
       @search = User.near(current_user.location, params[:within].to_i).search(params[:q])
     else
       @search = User.search(params[:q])
-
     end
+
     @members = @search.result.page(params[:page]).per(10)
-    @users = User.all
+    @users = @members.where.not(latitude: nil, longitude: nil)
     @hash = Gmaps4rails.build_markers(@users) do |user, marker|
-      marker.lat user.latitude
-      marker.lng user.longitude
+        marker.lat user.latitude
+        marker.lng user.longitude
     end
 
     @activities = Activity.all
