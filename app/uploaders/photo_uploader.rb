672d24daae7b10e40a4aa83d918ab53b441dbf99
macheteshuffle <jeff.li029@gmail.com>
2014-01-22 14:19:04 -0500

    fixed favorites path and carrierwave default pic issue

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -41,9 +41,8 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+     ActionController::Base.helpers.asset_path("fallback/" + [version_name, "no_profile.png"].compact.join('_'))
   #
-     "/assets/fallback/" + [version_name, "no_profile.png"].compact.join('_')
   end
 
   # Process files as they are uploaded:
