84aa295500fdde8f9405ad696f0471d5975e1f06
macheteshuffle <jeff.li029@gmail.com>
2014-01-22 15:15:00 -0500

    fixing favoriting a user

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,7 +1,7 @@
 class Favorite < ActiveRecord::Base
   validates_presence_of :user_id
   validates_presence_of :favorited_id
-  validates_uniqueness_of :favorited_id
+  validates_uniqueness_of :favorited, scope: :user
 
   belongs_to :user,
     inverse_of: :favorites
