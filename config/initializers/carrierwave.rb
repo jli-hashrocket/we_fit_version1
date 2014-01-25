278371e0722d5daf60e18e65783d47db0c0930c6
macheteshuffle <jeff.li029@gmail.com>
2014-01-24 19:10:00 -0500

    moved AWS keys to env file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: "AKIAI3NWUDVRLQVKJIIA",
-    aws_secret_access_key: "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"
+    aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+    aws_secret_access_key: ENV['AWS_SECRET_KEY']
   }
   config.fog_directory  = "wefit-#{Rails.env}"
 end
