05635a20096234375f9c34385e9f18985a90ab03
macheteshuffle <jeff.li029@gmail.com>
2014-03-29 12:45:21 -0400

    changed json to return only the results of search query

--- app/assets/javascripts/select.js
+++ app/assets/javascripts/select.js
@@ -10,26 +10,26 @@ $("#message_recipient_id").select2({
     dataType: 'json',
     data: function (term, page) {
       return {
-        term: term,
+        q: term,
         page_limit: 10
       };
     },
     results: function(data, page){
-      return { results: $.map( data, function(member, i) {
+      return { results: $.map( data, function(member) {
         return { id: member.id, text: member.username };
       } ) };
     }
-  },
-  initSelection: function(element, callback) {
-    var id = $(element).val();
-      $.ajax("../to_json",{
-        data: [{ id: id }],
-        dataType: 'json',
-      }).done(function(data) {
-        callback(data.result[0])
-      });
-  },
-  formatResult: format,
-  formatSelection: format
+  }
+  // initSelection: function(element, callback) {
+  //   var id = $(element).val();
+  //     $.ajax("../to_json",{
+  //       data: [{ id: id }],
+  //       dataType: 'json',
+  //     }).done(function(data) {
+  //       console.log("hello");
+  //     });
+  // },
+  // formatResult: format,
+  // formatSelection: format
 
 });
